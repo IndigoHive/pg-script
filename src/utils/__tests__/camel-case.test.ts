4f2e3e5f71b8ec7ef06475513fe07276db94b2bb
Fernando Kuteken <fskuteken@gmail.com>
2024-09-23 21:02:20 -0300

    Fix camelCase method

--- src/utils/__tests__/camel-case.test.ts
+++ src/utils/__tests__/camel-case.test.ts
@@ -4,5 +4,6 @@ import { describe, expect, test } from 'vitest'
 describe('camelCase', () => {
   test('converts text to camel case', () => {
     expect(camelCase('user_id')).toBe('userId')
+    expect(camelCase('userId')).toBe('userId')
   })
 })
--- src/utils/camel-case.ts
+++ src/utils/camel-case.ts
@@ -1,11 +1,5 @@
 export function camelCase (string: string) {
-  return string
-    .toLowerCase()
-    .replace(/([-_][a-z])/g,
-      group =>
-      group
-        .toUpperCase()
-        .replace('-', '')
-        .replace('_', '')
-  )
+	return string.replace(/[_.-](\w|$)/g, function (_, x) {
+		return x.toUpperCase()
+	})
 }
